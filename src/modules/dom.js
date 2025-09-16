861757d97431ff81550bb70ff46b0ea793eaa792
TipDevs <tipdevs@gmail.com>
2025-09-16 13:36:31 +0100

    feat(dom): includes the following feature: - shows player details and AI when game start - update the game console with player ship visible at game start - update the game console with AI ships hidden at game start - update game console with player board state while keeping:       - player ships visible       - AI ships hidden       - add hit indicator class       - add miss indicator class

---
+++ src/modules/dom.js
@@ -0,0 +1,34 @@
+export const DOM = (() => {
+  const playerContainer = document.querySelector("#player > .game-pad");
+  const spotAiContainer = document.querySelector("#spotAi > .game-pad");
+  function renderBoard(board, container, isAi = false) {
+    container.innerHTML = "";
+    const grid = board.getBoard();
+    console.log(grid);
+    grid.forEach((row, r) => {
+      row.forEach((cell, c) => {
+        const div = document.createElement("div");
+        div.dataset.row = r;
+        div.dataset.col = c;
+        if (!isAi && cell && typeof cell.getShipLength === "function") {
+          div.classList.add("ship");
+        }
+
+        if (cell === "miss") div.classList.add("miss");
+        if (cell === "hit") div.classList.add("hit");
+        container.appendChild(div);
+      });
+    });
+  }
+  const startGame = (playerName, spotAiName) => {
+    const title_indicator = document.querySelector("#title-indicator");
+    const game_console = document.querySelector("#game-console");
+    const game_start = document.querySelector("#game-start");
+    document.querySelector("#player > h2").textContent = playerName;
+    document.querySelector("#spotAi > h2").textContent = spotAiName;
+    game_start.style.display = "none";
+    title_indicator.style.display = "flex";
+    game_console.style.display = "flex";
+  };
+  return { renderBoard, startGame, playerContainer, spotAiContainer };
+})();
