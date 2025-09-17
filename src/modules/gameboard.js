e861f348c65f79f9fab01e2cbe6e3999ac074724
TipDevs <tipdevs@gmail.com>
2025-09-17 19:06:37 +0100

    feat(update): add new methods for ai randomship placement - refactor to handle errors

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,25 +1,64 @@
 export class Gameboard {
   #size = 10;
   #ships = [];
+  #hitAttacks = [];
   #missedAttacks = [];
   #board = Array(this.#size)
     .fill(null)
     .map(() => Array(this.#size).fill(null));
   placeShip(ship, [row, col], direction) {
+    const usedCoords = new Set(
+      this.#ships.flatMap((port) =>
+        port.coordinates.map((coord) => coord.toString())
+      )
+    );
     const coords = [];
+    const size = this.#board.length; // assuming square board
     if (direction === "horizontal") {
+      if (col + ship.getShipLength() > size) {
+        throw new Error("out of bounds");
+      }
       for (let i = 0; i < ship.getShipLength(); i++) {
+        if (usedCoords.has([row, col + i].toString())) {
+          throw new Error("collision: coordinate has been filled");
+        }
         this.#board[row][col + i] = ship;
         coords.push([row, col + i]);
       }
     } else {
+      if (row + ship.getShipLength() > size) {
+        throw new Error("out of bounds");
+      }
       for (let i = 0; i < ship.getShipLength(); i++) {
+        if (usedCoords.has([row + i, col].toString())) {
+          throw new Error("collision: coordinate has been filled");
+        }
         this.#board[row + i][col] = ship;
         coords.push([row + i, col]);
       }
     }
     this.#ships.push({ ship, coordinates: coords });
   }
+  placeRandomShip(board, ship) {
+    const directions = ["horizontal", "vertical"];
+    const size = board.getBoard().length;
+    let placed = false;
+
+    while (!placed) {
+      const row = Math.floor(Math.random() * size);
+      const col = Math.floor(Math.random() * size);
+      const direction =
+        directions[Math.floor(Math.random() * directions.length)];
+
+      try {
+        board.placeShip(ship, [row, col], direction);
+        placed = true;
+      } catch (err) {
+        // Retry on collision or out of bounds
+      }
+    }
+  }
+
   receiveAttack([row, col]) {
     const attackedCell = this.#board[row][col];
     if (attackedCell === null) {
@@ -31,6 +70,7 @@ export class Gameboard {
     if (typeof attackedCell?.hit === "function") {
       attackedCell.hit();
       this.#board[row][col] = "hit";
+      this.#hitAttacks.push([row, col]);
       return "hit";
     }
 
@@ -44,5 +84,8 @@ export class Gameboard {
   }
   retrieveMissedAttacks() {
     return [...this.#missedAttacks];
+  }
+  retrieveHitAttacks() {
+    return [...this.#hitAttacks];
   }
 }
